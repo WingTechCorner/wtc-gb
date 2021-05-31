d30a6872834171c4b15a28aeaa1275e4772f10cf
Wing Wong <wingedpower@gmail.com>
2021-05-31 12:47:35 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: "WingTechCorner",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "https://wingtechcorner.com/graphql-zedzedalpha",
+      },
+    },
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "GTM-5NXXNDL",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
